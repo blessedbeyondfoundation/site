590edd2449551b1c95acd222b22b737eb1b197ef
blessedbeyondfoundation <100896923+blessedbeyondfoundation@users.noreply.github.com>
2024-07-23 11:57:39 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://feature-tour.netlify.app',
+  site: 'https://blessedbeyondfoundation.com',
+  meta: {
+    siteName: 'Blessed Beyond Foundation'
+  },
   integrations: [
     preact(),
     sitemap({
-      canonicalURL: 'https://feature-tour.netlify.app'
+      canonicalURL: 'https://blessedbeyondfoundation.com'
     })
   ],
 });
